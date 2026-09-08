4ca6ef1e8ab577a438cc364467a8c93a97ab9616
Hemachandru-BCA <2006hemachandru@gmail.com>
2026-09-08 13:03:12 +0530

    made som cchanges

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig(({ mode }) => {
 
     const port = Number(env.PORT) || 5173;
     const basePath = env.BASE_PATH || '/';
-    const backend = env.VITE_API_URL || 'https://thupparivu-sih26189.onrender.com';
+    const backend = env.VITE_API_URL || 'http://localhost:3000';
 
     return {
         base: basePath,
